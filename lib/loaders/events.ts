4ec8f93676c7f7234ac1d0a0db709be457660a43
JenniferTantely <hei.jennifer.2@gmail.com>
2025-03-21 10:27:13 +0300

    feat(api): add upcoming=true filter to get future events

--- lib/loaders/events.ts
+++ lib/loaders/events.ts
@@ -1,14 +1,21 @@
-export async function getUpcomingEvents() {
+export async function getUpcomingEvents(page: number = 1, pageSize: number = 10) {
     const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+    try {
+        const res = await fetch(`${API_BASE_URL}/event?upcoming=true&page=${page}&pageSize=${pageSize}`, {
+            next: { revalidate: 60 }, // ISR: Rafraîchi toutes les 60s
+        });
+        
+            // // Simuler un délai de chargement (par exemple 5 secondes)
+            // await new Promise(resolve => setTimeout(resolve, 5000));
 
-    // // Simuler un délai de chargement (par exemple 5 secondes)
-    // await new Promise(resolve => setTimeout(resolve, 5000));
+        if (!res.ok) {
+            console.error("API Error:", res.status, res.statusText);
+            throw new Error(`Failed to fetch events: ${res.status} ${res.statusText}`);
+        }
 
-    const res = await fetch(`${API_BASE_URL}/event?upcoming=true?page=1&pageSize=10`, {
-        next: { revalidate: 60 }, // ISR: Rafraîchi toutes les 60s
-    });
-
-    if (!res.ok) throw new Error("Failed to fetch events");
-
-    return res.json();
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.error("Error in getUpcomingEvents:", error);
+    }
 }
