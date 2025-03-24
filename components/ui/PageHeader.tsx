26ad99ee007e8d7b5e88e6aff7d3f0dfab1da8eb
JenniferTantely <hei.jennifer.2@gmail.com>
2025-03-24 23:06:19 +0300

    fix(PageHeader): remove title props

--- components/ui/PageHeader.tsx
+++ components/ui/PageHeader.tsx
@@ -1,18 +1,16 @@
 // /components/ui/PageHeader.tsx
-import Link from "next/link";
 
 interface PageHeaderProps {
-  title: string;
   count: number;
 }
 
-export default function PageHeader({ title, count}: PageHeaderProps) {
+export default function PageHeader({ count }: PageHeaderProps) {
   return (
     <>
       {/* Header section */}
-      <div className="mb-4 flex items-center justify-between">
+      <div className="mb-4 mt-20 flex items-center justify-between">
         <h1 className="text-xl font-bold dark:text-white">
-          {title}
+          Tous les événements
         </h1>
         <span className="text-sm text-gray-500 dark:text-gray-400">
           {count} événements disponibles
