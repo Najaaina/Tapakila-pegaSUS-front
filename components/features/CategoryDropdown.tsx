1a0dfb8b21727a695dbe6071dd00bdb5e8e99ada
JenniferTantely <hei.jennifer.2@gmail.com>
2025-03-24 23:14:30 +0300

    fix(CategoryDropdown): create a component with CategoryDropdown for clean code

---
+++ components/features/CategoryDropdown.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+type CategoryDropdownProps = {
+    uniquecategories: string[];
+    selectedCategory: string;
+    setSelectedCategory: (category: string) => void;
+}
+
+export default function CategoryDropdown({uniquecategories, selectedCategory, setSelectedCategory} : CategoryDropdownProps) {
+  return (
+    <select
+      className="border rounded-md p-2"
+      value={selectedCategory}
+      onChange={(e) => setSelectedCategory(e.target.value)}
+    >
+      <option value="">Tous les catégories</option>
+      {uniquecategories.map((category) => (
+        <option key={category} value={category}>
+          {category}
+        </option>
+      ))}
+    </select>
+  );
+}
